15686ee220e15973b0ab52ab863a59d15e4ecefc
JSpeedie <julianspeedie@gmail.com>
2020-06-12 23:00:31 -0400

    Image uploading now works

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ app.use(function (req, res, next){
     next();
 });
 
-/*
- *  Image:
+let ImageId = (function() {
+	let id = 0;
+
+	return function image() {
+		this._id = id++;
+	}
+}());
+
+/*  Image:
  * ========
  * - title
  * - author
@@ -28,15 +35,58 @@ app.use(function (req, res, next){
  * - date_taken
  */
 
-/* ========================================= */
-/* Image addition, retrieval, and deletion */
-/* ========================================= */
 
 // Add new image to gallery
-// TODO: needs checking for if it uploads the image itself properly
-app.post('/api/images/', upload.single('picture'), function (req, res, next) {
-	images.insert({title: req.body.image_title, author: req.body.author_name, image: req.image_file, description: req.image_description, date_taken: req.date_taken}, function(err, newDoc) {});
+app.post('/api/images/', upload.single('image_file'), function (req, res, next) {
+	/* Get an ID for the new image */
+	let newImageId = new ImageId();
+	/* Create storage object */
+	let new_image_obj = {
+		_id: newImageId._id,
+		/* req.body.x must have x match the element names in the form
+		 * in the HTML */
+		title: req.body.image_title,
+		date_taken: req.body.image_date_taken,
+		description: req.body.image_description,
+		author_name: req.body.image_author_name,
+		/* This must be req.file as opposed to req.body.x or something */
+		image: req.file
+		};
+	console.log(new_image_obj);
+	/* Store object in database */
+	images.insert(new_image_obj, function(err, newDoc) {});
+	// Respond to requester
+	//res.json(newImage);
+
 	return res.redirect('/');
+});
+
+// Retrieve given image from gallery
+app.get('/api/images/:id/', function (req, res, next) {
+	let search = {
+		_id: parseInt(req.params.id)
+	};
+
+	images.findOne(search, function(err, doc) {
+		/* If an image of the given id exists, return that image */
+		if (doc != null) {
+			/* doc.x must match name in object inserted into Datastore */
+			res.setHeader('Content-Type', doc.image.mimetype);
+			res.sendFile(doc.image.path, { root: __dirname });
+		} else {
+			console.error("ERROR: image does not exist");
+			return res.status(404).end("Image: " + req.params.id + " did not exist");
+		}
+	});
+});
+
+// TODO: require authorization for this
+// Retrieve all images
+app.get('/api/images/', function (req, res, next) {
+	images.find({}).exec(function(err, data) {
+		console.log(data); // TODO: remove
+		return res.json(data);
+	});
 });
 
 const http = require('http');
--- static/js/api.js
+++ static/js/api.js
@@ -1,6 +1,8 @@
 var api = (function(){
 	var module = {};
 
+    let imageListeners = [];
+
 	/* Helper function for sending data */
 	function send(method, url, data, callback) {
 
@@ -21,13 +23,63 @@ var api = (function(){
 		}
 	}
 
+	/* Helper function for sending images */
+	function sendImage(method, url, data, callback) {
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.onload = function() {
+			if (xhr.status !== 200) callback("[" + xhr.status + "]" + xhr.responseText, null);
+			else callback(null, JSON.parse(xhr.responseText));
+		};
+
+		xhr.open(method, url, true);
+
+		if (!data) {
+			xhr.send();
+		} else {
+			xhr.send(data);
+		}
+	}
+
 	/* Add an image to the gallery */
-	module.addImage = function(image_title, author_name, image_description, image_file, date_taken){
-		// TODO: fix this call. How to send image too?
-		send("POST", "/api/images/", {username: username, content: content}, file, function(err, res){
-			 if (err) return notifyErrorListeners(err);
-			 notifyImageListeners();
+	module.addImage = function(image_title, author_name, image_description, image_file, date_taken, callback) {
+		let image_info = {
+			title: image_title,
+			date_taken: date_taken,
+			description: image_description,
+			author: author_name
+		}
+		console.log("about to send POST");
+		send("POST", "/api/images/", image_info, image_file, function(err, res){
+			// TODO: fix
+			//if (err) return notifyErrorListeners(err);
+			//callback(res);
+			notifyImageListeners();
 		});
+	}
+
+	function notifyImageListeners(){
+		// getImages(function(err, images){
+		// 	if (err) return notifyErrorListeners(err);
+		// 	imageListeners.forEach(function(listener){
+		// 		listener(images);
+		// 	});
+		// });
+			imageListeners.forEach(function(listener){
+				listener(images);
+			});
+	}
+
+	// register an image listener
+	// to be notified when an image is added or deleted from the gallery
+	module.onImageUpdate = function(listener){
+		// imageListeners.push(listener);
+		// getImages(function(err, images){
+		// 	if (err) return notifyErrorListeners(err);
+		// 	listener(images);
+		// });
+		listener(null);
 	}
 
 	return module;
