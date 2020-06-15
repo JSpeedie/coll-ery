88938eb393450cf00ec673fc85b0175978af9879
JSpeedie <julianspeedie@gmail.com>
2020-06-15 15:35:39 -0400

    Began work on server api for collections

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var multer = require('multer');
 var upload = multer({dest: 'uploads/'});
 
 var Datastore = require('nedb')
+	, collections = new Datastore({ filename: 'db/collections.db', autoload: true });
 	, images = new Datastore({ filename: 'db/images.db', autoload: true });
 
 app.use(express.static('static'));
@@ -17,6 +18,16 @@ app.use(function (req, res, next){
     console.log("HTTP request", req.method, req.url, req.body);
     next();
 });
+
+/*  Image:
+ * ========
+ * - _id
+ * - title
+ * - author
+ * - image
+ * - description
+ * - date_taken
+ */
 
 let ImageId = (function() {
 	let id = 1;
@@ -36,17 +47,36 @@ let ImageId = (function() {
 	}
 }());
 
-/*  Image:
+/*  Collection:
  * ========
+ * - _id
  * - title
- * - author
- * - image
  * - description
- * - date_taken
+ * - images[]
  */
 
+let CollectionId = (function() {
+	let id = 1;
+	/* Read the number of collections on the server/in storage and adjust the next
+	 * id as needed */
+	collections.count({}, function (err, count) {
+		if (err) {
+			console.log("There was an error counting the number of collections in the system");
+		} else {
+			id = count + 1;
+			console.log("The next id for an collection will be: " + id);
+		}
+	});
 
-// Add new image to gallery
+	return function collection() {
+		console.log("The id for this collection will be: " + id);
+		this._id = id++;
+		console.log("The next id for an collection will be: " + id);
+	}
+}());
+
+
+/* Add new image to gallery */
 app.post('/api/images/', upload.single('image_file'), function (req, res, next) {
 	/* Get an ID for the new image */
 	let newImageId = new ImageId();
@@ -93,6 +123,65 @@ app.get('/api/images/:id/', function (req, res, next) {
 // Retrieve all images
 app.get('/api/images/', function (req, res, next) {
 	images.find({}).exec(function(err, data) {
+		return res.json(data);
+	});
+});
+
+
+
+/* Add new collection to gallery */
+app.post('/api/collections/', function (req, res, next) {
+	/* Get an ID for the new collection */
+	let newCollectionId = new CollectionId();
+
+	// TODO: translate content to a json array for storage
+	new_collection_images = req.body.images;
+	console.log("new_collection_images:");
+	console.log(new_collection_images);
+	if (new_collection_images == null) {
+		new_collection_images = {};
+	}
+
+	/* Create storage object */
+	let new_collection_obj = {
+		_id: newCollectionId._id,
+		/* req.body.x must have x match the element names in the form
+		 * in the HTML */
+		title: req.body.collection_title,
+		description: req.body.collection_description,
+		images: new_collection_images
+	};
+	console.log("inserting new collection with id of " + newCollectionId._id);
+	/* Store object in database */
+	collections.insert(new_collection_obj, function(err, newDoc) {});
+	// Respond to requester
+	//res.json(newCollection);
+
+	return res.redirect('/');
+});
+
+
+/* Retrieve given collection from gallery */
+app.get('/api/collections/:id/', function (req, res, next) {
+	let search = {
+		_id: parseInt(req.params.id)
+	};
+
+	collections.findOne(search, function(err, doc) {
+		/* If an collection of the given id exists, return that collection */
+		if (doc != null) {
+			res.json(doc);
+		} else {
+			console.error("ERROR: collection does not exist");
+			return res.status(404).end("Collection: " + req.params.id + " did not exist");
+		}
+	});
+});
+
+// TODO: require authorization for this
+// Retrieve all collections
+app.get('/api/collections/', function (req, res, next) {
+	collections.find({}).exec(function(err, data) {
 		return res.json(data);
 	});
 });
