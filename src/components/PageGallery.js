b4c10072a42cb155853ad8506e6e68aca850735f
JSpeedie <julianspeedie@gmail.com>
2020-06-30 22:32:43 -0400

    Redefined the gallery page as a React component

---
+++ src/components/PageGallery.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+import ImageGallery from './ImageGallery';
+import CollectionGallery from './CollectionGallery';
+import NavBar from './NavBar';
+
+class PageGallery extends Component{
+
+	constructor(props) {
+		super(props);
+	}
+
+	render(){
+		return(
+	       <div className="Gallery">
+             <header>
+                 <a href="/" id="title">Gallery</a>
+             </header>
+
+             <NavBar />
+
+             <br/>
+             <h2>Images</h2>
+             <br/>
+
+             <ImageGallery />
+
+             <br/>
+             <h2>Collections</h2>
+             <br/>
+
+             <CollectionGallery />
+
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+             <br/>
+           </div>
+		);
+	}
+}
+
+export default PageGallery;
