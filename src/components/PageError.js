198301b532dcce755de914defeabae7cf67657ac
JSpeedie <julianspeedie@gmail.com>
2020-06-30 22:33:08 -0400

    Added an error page for 404s

---
+++ src/components/PageError.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+import NavBar from './NavBar';
+
+class PageError extends Component{
+
+	constructor(props) {
+		super(props);
+	}
+
+	render(){
+		return(
+	       <div className="Error">
+             <header>
+                 <a href="/" id="title">Gallery</a>
+             </header>
+
+             <NavBar />
+
+             <br/>
+             <h2>There was an error. Please enter a valid url</h2>
+             <br/>
+           </div>
+		);
+	}
+}
+
+export default PageError;
