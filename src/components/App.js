b1c59f2a2ce7b9a2edaf892aa17e10f237e1c3d4
JSpeedie <julianspeedie@gmail.com>
2020-06-30 22:28:57 -0400

    Added React App file

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import PageGallery from './PageGallery';
+import PageCollectionAdd from './PageCollectionAdd';
+import PageImageView from './PageImageView';
+import PageError from './PageError';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+          <div className="App">
+			<Switch>
+              <Route path='/' component={PageGallery} exact />
+              <Route path='/collection/add' component={PageCollectionAdd} />
+              <Route path='/image/:id' component={PageImageView} />
+              <Route component={PageError} />
+            </Switch>
+          </div>
+		);
+	}
+}
+
+export default App;
