0b65ef522ad01e0af4b09f8e4dbaf9dc9b8c86fa
JSpeedie <julianspeedie@gmail.com>
2020-07-02 17:22:10 -0400

    Added a "load more" button to ImageGallery

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -10,27 +10,64 @@ class ImageGallery extends Component{
 			sliceStart: 0,
 			sliceEnd: 10
 		}
+		this.increaseSlice = this.increaseSlice.bind(this);
+	}
+
+	increaseSlice() {
+		if (this.state.sliceEnd <= this.state.images.length) {
+			this.setState(prevState => ({
+				sliceEnd: prevState.sliceEnd + 10
+			}));
+		}
 	}
 
 	render() {
 		return(
           <div className="image_gallery">
-            {this.state.images.slice(this.state.sliceStart, this.state.sliceEnd).map((img) => 
-                <GalleryItem key={img._id}
-                             imageId={img._id}
-                             previewImageId={img._id}
-                             description={img.description}
-                             title={img.title}/>)
-            }
+            <div className="image_gallery_images">
+              {this.state.images.slice(this.state.sliceStart, this.state.sliceEnd).map((img) => 
+                  <GalleryItem key={img._id}
+                               imageId={img._id}
+                               previewImageId={img._id}
+                               description={img.description}
+                               title={img.title}/>)
+              }
+            </div>
+            {(this.state.sliceEnd <= this.state.images.length)
+              ? 
+                <div className="image_gallery_lower_controls">
+                  <button className="image_gallery_controls_button" onClick={this.increaseSlice}>
+                    Load more
+                  </button>
+                </div>
+              : ""}
           </div>
 		);
 	}
 
 	componentDidMount() {
-		fetch('http://localhost:3000/api/images')
-			.then(res => res.json()).then((data) => {
-				this.setState({ images: data })
-			}).catch(console.log);
+		/* If a collectionId is specified, render all the images from the
+		 * collection, otherwise render every image */
+		if (this.props.collectionId) {
+			fetch('http://localhost:3000/api/collections/' + this.props.collectionId)
+				.then(res => res.json()).then((data) => {
+					let imageIds = data.images;
+
+					for (let i = 0; i < imageIds.length; i++) {
+						fetch('http://localhost:3000/api/images/' + imageIds[i])
+							.then(res => res.json()).then((data) => {
+								this.setState(prevState => ({
+									images: [...prevState.images, data]
+								}))
+							}).catch(console.log);
+					}
+				}).catch(console.log);
+		} else {
+			fetch('http://localhost:3000/api/images')
+				.then(res => res.json()).then((data) => {
+					this.setState({ images: data });
+				}).catch(console.log);
+		}
 	}
 }
 
