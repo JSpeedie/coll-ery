58ba6ff386a9e564a5957fc52a857c0a6865a6ce
JSpeedie <julianspeedie@gmail.com>
2020-07-02 23:04:56 -0400

    Changed allowed origin setup to allow for multiple domains

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ app.use(express.static('static'));
 
 app.use(function (req, res, next){
 	console.log("HTTP request", req.method, req.url, req.body);
-	res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+	let allowedOrigins = [ "http://localhost:8000" ]
+	let origin = req.headers.origin;
+
+	if (allowedOrigins.indexOf(origin) > -1) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
