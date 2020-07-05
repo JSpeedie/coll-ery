bb3240afc8491ff266ef7a2c6e3617f8be8f72b8
JSpeedie <julianspeedie@gmail.com>
2020-07-05 10:30:46 -0400

    Styled edit image page to have fields to the right of the image

--- src/components/PageImageEdit.js
+++ src/components/PageImageEdit.js
@@ -30,39 +30,39 @@ class PageImageEdit extends Component{
              <h2>{this.state.image.title}</h2>
              <br/>
 
-             <div id="image_section">
-               <div className="view_image_container">
+             <div className="edit_image_section">
+               <div className="edit_image_container">
                  <div className="edit_image_preview">
-                   <img className="view_image_preview_image" src={"http://localhost:3000/api/img/" + this.state.image._id} height="400" />
-                   <div className="view_image_info_div">
-                     <div className="edit_image_info_title">
-                       <p>
-                         Title:
-                       </p>
-                       <input type="text" className="form_element edit_image_info_title_input" value={this.state.image.title}/>
-                     </div>
-                     <div className="edit_image_info_author_name">
-                       <p>
-                         Author name:
-                       </p>
-                       <input type="text" className="form_element edit_image_info_author_name_input" value={this.state.image.author_name}/>
-                     </div>
-                     <div className="edit_image_info_description">
-                       <p>
-                         Description:
-                       </p>
-                       <input type="text" className="form_element edit_image_info_description_input" value={this.state.image.description}/>
-                     </div>
-                     <div className="edit_image_info_date_taken">
-                       <p>
-                         Date taken:
-                       </p>
-                       <input type="date" className="form_element edit_image_info_date_taken_input" value={this.state.image.date_taken}/>
-                     </div>
-                     <button className="edit_image_info_save">
-                       Save
-                     </button>
+                   <img className="edit_image_preview_image" src={"http://localhost:3000/api/img/" + this.state.image._id} height="400" />
+                 </div>
+                 <div className="edit_image_edit_fields_div">
+                   <div className="edit_image_info_title">
+                     <p>
+                       Title:
+                     </p>
+                     <input type="text" className="form_element edit_image_info_title_input" value={this.state.image.title}/>
                    </div>
+                   <div className="edit_image_info_author_name">
+                     <p>
+                       Author name:
+                     </p>
+                     <input type="text" className="form_element edit_image_info_author_name_input" value={this.state.image.author_name}/>
+                   </div>
+                   <div className="edit_image_info_description">
+                     <p>
+                       Description:
+                     </p>
+                     <input type="text" className="form_element edit_image_info_description_input" value={this.state.image.description}/>
+                   </div>
+                   <div className="edit_image_info_date_taken">
+                     <p>
+                       Date taken:
+                     </p>
+                     <input type="date" className="form_element edit_image_info_date_taken_input" value={this.state.image.date_taken}/>
+                   </div>
+                   <button className="edit_image_info_save">
+                     Save
+                   </button>
                  </div>
                </div>
              </div>
