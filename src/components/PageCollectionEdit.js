647f008adcfd2887b09893d316b76b0446dfa389
JSpeedie <julianspeedie@gmail.com>
2020-07-08 19:26:07 -0400

    Updated HTML to use thumbnail images and have the correct fields

--- src/components/PageCollectionEdit.js
+++ src/components/PageCollectionEdit.js
@@ -42,54 +42,58 @@ class PageCollectionEdit extends Component{
              <NavBar />
 
              <br/>
-             <h2>{this.state.collection.title}</h2>
+             <h2>{this.state.title}</h2>
              <br/>
 
              <div id="collection_section">
-               <div className="view_collection_container">
-                 <div className="view_collection_preview">
-                   <img className="view_collection_preview_collection" src={"http://localhost:3000/api/img/" + this.state.collection.thumbnail_image_id} height="400" />
-                   <div className="view_collection_info_div">
-                     <div className="edit_collection_info_title">
-                       <p>
-                         Title:
-                       </p>
-                       <input type="text" className="form_element edit_collection_info_title_input" value={this.state.collection.title}/>
-                     </div>
-                     <div className="edit_collection_info_author_name">
-                       <p>
-                         Author name:
-                       </p>
-                       <input type="text" className="form_element edit_collection_info_author_name_input" value={this.state.collection.author_name}/>
-                     </div>
-                     <div className="edit_collection_info_description">
-                       <p>
-                         Description:
-                       </p>
-                       <input type="text" className="form_element edit_collection_info_description_input" value={this.state.collection.description}/>
-                     </div>
-                     <div className="edit_collection_info_date_taken">
-                       <p>
-                         Date taken:
-                       </p>
-                       <input type="date" className="form_element edit_collection_info_date_taken_input" value={this.state.collection.date_taken}/>
-                     </div>
-                     <div className="edit_collection_info_thumbnail_image_id">
-                       <p>
-                         Thumbnail Image ID:
-                       </p>
-                       <input type="text" className="form_element edit_collection_info_thumbnail_image_id_input" value={this.state.collection.thumbnail_image_id}/>
-                     </div>
-                     <input type="text" className="form_element edit_collection_info_images_input" value={this.state.collection.images}/>
-                     <button className="edit_collection_info_save">
-                       Save
-                     </button>
-                     <button className="edit_collection_info_add_images">
-                       Add Images
-                     </button>
+               <div className="edit_collection_container">
+                 <div className="edit_collection_preview">
+                   <img className="edit_collection_preview_collection"
+                        src={"http://localhost:3000/api/img/" + this.state.thumbnail_image_id}
+                        height="400" />
+                 </div>
+                 <div className="edit_collection_edit_fields_div">
+                   <div className="edit_collection_info_title">
+                     <p>
+                       Title:
+                     </p>
+                     <input type="text"
+                            className="form_element edit_collection_info_title_input"
+                            value={this.state.title}
+                            onChange={this.handleTitleChange}/>
                    </div>
+                   <div className="edit_collection_info_description">
+                     <p>
+                       Description:
+                     </p>
+                     <input type="text"
+                            className="form_element edit_collection_info_description_input"
+                            value={this.state.description}
+                            onChange={this.handleDescriptionChange}/>
+                   </div>
+                   <div className="edit_collection_info_thumbnail_image_id">
+                     <p>
+                       Thumbnail Image ID:
+                     </p>
+                     <input type="text"
+                            className="form_element edit_collection_info_thumbnail_image_id_input"
+                            value={this.state.thumbnail_image_id}
+                            onChange={this.handleThumbnailIdChange}/>
+                   </div>
+                   <input type="text"
+                          className="form_element edit_collection_info_images_input"
+                          value={this.state.images}
+                            onChange={this.handleImagesChange}/>
+                   <br/>
+                   <button className="edit_collection_info_save button">
+                     Save
+                   </button>
+                   <button className="edit_collection_info_add_images button">
+                     Add Images
+                   </button>
                  </div>
                </div>
+               <ImageChooser collectionId={this.props.match.params.id} />
              </div>
 
              <br/>
