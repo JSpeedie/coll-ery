e88cb2f7710db063b1d1999340841e3046dc259d
JSpeedie <julianspeedie@gmail.com>
2020-07-11 23:12:28 -0400

    Added collection controls at the top of page

--- src/components/PageCollectionView.js
+++ src/components/PageCollectionView.js
@@ -9,7 +9,23 @@ class PageCollectionView extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
+			_id: "_id",
 			title: "title"
+		}
+
+		this.deleteItem = this.deleteItem.bind(this);
+	}
+
+	deleteItem() {
+		if (window.confirm("Are you sure you want to delete the collection, \"" + this.state.title + "\"?")) {
+			const requestOptions = {
+				method: 'DELETE'
+			};
+
+			fetch("http://localhost:3000/api/collections/" + this.state._id, requestOptions)
+				.then((response) => {
+					return response.json();
+				}).catch(console.log);
 		}
 	}
 
@@ -25,6 +41,17 @@ class PageCollectionView extends Component{
              <br/>
              <h2>{this.state.title}</h2>
              <br/>
+
+             <div className="collection_view_controls">
+               <Link className="gallery_item_title_control"
+                     to={"/collection/edit/" + this.state._id}>
+                 Edit
+               </Link>
+               <div className="gallery_item_title_control"
+                    onClick={this.deleteItem}>
+                 Delete
+               </div>
+             </div>
 
              <ImageGallery collectionId={this.props.match.params.id}/>
 
@@ -58,7 +85,10 @@ class PageCollectionView extends Component{
 		/* Get the title of the collection */
 		fetch('http://localhost:3000/api/collections/' + this.props.match.params.id)
 			.then(res => res.json()).then((data) => {
-				this.setState({ title: data.title });
+				this.setState({
+					_id: data._id,
+					title: data.title
+				});
 			}).catch(console.log);
 	}
 }
