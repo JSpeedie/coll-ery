1bfb09b3ef71ea76a0dc60f89619f9062f844b32
JSpeedie <julianspeedie@gmail.com>
2020-07-11 23:17:28 -0400

    Removed some unncecessary print statements

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var multer = require('multer');
@@ -120,7 +120,6 @@ app.get('/api/images/:id/', function (req, res, next) {
 								+ req.params.id);
 							reject(err)
 						} else {
-							console.log(data);
 							resolve(data[0]);
 						}
 					});
@@ -506,7 +505,6 @@ app.get('/api/collections/:id/', function (req, res, next) {
 								+ req.params.id);
 							reject(err)
 						} else {
-							console.log(data);
 							resolve(data[0]);
 						}
 					});
